dd74c83f578a4f72dc89c0f18df3165a1b5b41fd
rafa <rafa@172.19.2.78>
2018-02-27 14:12:05 -0600

    Activación del Ngroute

--- Observatorio/WebContent/app/components/main/main.controller.js
+++ Observatorio/WebContent/app/components/main/main.controller.js
@@ -1,272 +1,73 @@
-var app = angular.module('observatorio', ['chart.js','smart-table','ui.grid','ui.grid.pinning','ngUtilidades']);
-app.controller('mainController',['$rootScope','$scope','$http', 
-	function($rootScope,$scope,$http){
-	var mi = this;
-	var fecha = new Date();
-	mi.anio = fecha.getFullYear();
-	mi.mes = fecha.getMonth() + 1;
-	mi.etiquetaX = "";
-	mi.labels = [];
-	mi.data = [];
-	
-	mi.ano_p_ejecucion = mi.anio;
-	mi.ano_p_ejecucion_1 = mi.anio-1;
-	mi.ano_p_ejecucion_2 = mi.anio-2;
-	mi.ano_p_ejecucion_3 = mi.anio-3;
-	mi.ano_p_ejecucion_4 = mi.anio-4;
-	mi.tot_p_ejecucion_4 = 0;
-	mi.tot_p_ejecucion_3 = 0;
-	mi.tot_p_ejecucion_2 = 0;
-	mi.tot_p_ejecucion_1 = 0;
-	mi.tot_p_ejecucion = 0;
-	mi.meses = ['Ene-','Feb-','Mar-','Abr-','May-','Jun-','Jul-','Ago-','Sep-','Oct-','Nov-','Dic-'];
-	
-	mi.entidad = 11130009;
-	mi.unidadEjecutora = 219;
-	mi.programa = 15;
-	mi.subPrograma = 0;
-	mi.actividad = 3;
-	mi.obra = 0;
-	
-	$http.post('/SEjecucionFisica',
-		{
-			accion: 'getEjecucionFisica',
-			entidad: mi.entidad,
-			unidadEjecutora: mi.unidadEjecutora,
-			programa: mi.programa,
-			subProbrama: mi.subPrograma,
-			actividad: mi.actividad,
-			obra: mi.obra,
-			t: new Date().getTime()
-		}).then(			
-		function(response){
-			if (response.data.success){
-				mi.dato = [];
-				mi.dato = response.data.ejecucionFisica;
-				
-				mi.rowCollection = [];
-				mi.rowCollection = mi.dato;
-				
-				mi.displayedCollection = [].concat(mi.rowCollection);
-				
-				if(mi.dato.length > 0){
-					for(var i=0; i<mi.dato.length;i++){
-						mi.tot_p_ejecucion_4 += mi.dato[i].p_ejecucion_4;
-						mi.tot_p_ejecucion_3 += mi.dato[i].p_ejecucion_3;
-						mi.tot_p_ejecucion_2 += mi.dato[i].p_ejecucion_2;
-						mi.tot_p_ejecucion_1 += mi.dato[i].p_ejecucion_1;
-						mi.tot_p_ejecucion += mi.dato[i].p_ejecucion;
-					}
-					
-					mi.tot_p_ejecucion_4 = ((mi.tot_p_ejecucion_4 / mi.dato.length)).toFixed(2);
-					mi.tot_p_ejecucion_3 = ((mi.tot_p_ejecucion_3 / mi.dato.length)).toFixed(2);
-					mi.tot_p_ejecucion_2 = ((mi.tot_p_ejecucion_2 / mi.dato.length)).toFixed(2);
-					mi.tot_p_ejecucion_1 = ((mi.tot_p_ejecucion_1 / mi.dato.length)).toFixed(2);
-					mi.tot_p_ejecucion = ((mi.tot_p_ejecucion / mi.dato.length)).toFixed(2);
-					
-					mi.getGraficaProducto({metaDescripcion:'General', entidad: mi.entidad, unidadEjecutora: mi.unidadEjecutora, programa: mi.programa, subPrograma: mi.subPrograma, actividad: mi.actividad, obra: mi.obra});
-				}
-			}
-		});
-	
-	mi.getGraficaProducto = function(row){
-		mi.tituloGrafica = row.metaDescripcion;
-		mi.getInfoGraficas(row);
-	}
-	
-	mi.getInfoGraficas = function(row){
-		mi.limpiarData();
-		$http.post('/SEjecucionFisica', 
-			{
-				accion : 'getEjecucionFisicaMensual',
-				entidad: row.entidad,
-				unidadEjecutora: row.unidadEjecutora,
-				programa: row.programa,
-				subProbrama: row.subPrograma,
-				actividad: row.actividad,
-				obra: row.obra,
-				codigo_meta: row.codigo_meta,
-				t: new Date().getTime()
-			}).then(function(response){
-				if (response.data.success){
-					var datoMensual = [];
-					datoMensual = response.data.ejecucionFisicaMensual;
-					
-					mi.series = ['% Ejecucion'];
-					mi.options.scales.xAxes["0"].scaleLabel.labelString = "Meses";
-					
-					var cantidades = [];
-					mi.datoAcumuladoMensual = [];
-					mi.datoAcumuladoAnual = [];
-					
-					var acumulado = 0;
-					
-					for(var i=0; i < datoMensual.length; i++){
-						if(mi.mes == datoMensual[i].mes && mi.anio == datoMensual[i].ejercicio)
-							break;
-						
-						if(datoMensual[i].mes == 1)
-							acumulado = 0;
-						else
-							acumulado = mi.datoAcumuladoMensual.length != 0 ? mi.datoAcumuladoMensual[i-1].p_ejecucion + datoMensual[i].p_ejecucion : datoMensual[i].p_ejecucion;
-						
-						mi.datoAcumuladoMensual.push({mes: datoMensual[i].mes, ejercicio: datoMensual[i].ejercicio, p_ejecucion: acumulado});
-						
-						if(datoMensual[i].mes == 12)
-							mi.datoAcumuladoAnual.push({ejercicio: datoMensual[i].ejercicio, p_ejecucion: acumulado});
-					}
-					
-					for(var i=0; i<mi.datoAcumuladoMensual.length; i++){
-						mi.labels.push(mi.meses[mi.datoAcumuladoMensual[i].mes - 1] + mi.datoAcumuladoMensual[i].ejercicio);
-						cantidades.push(Number(mi.datoAcumuladoMensual[i].p_ejecucion).toFixed(2));
-					}
-					
-					mi.data.push(cantidades);
-				}
-			});
-				
-			$http.post('/SEjecucionFisica', 
-				{
-					accion: 'getVectoresValores', 
-					entidad: row.entidad,
-					unidadEjecutora: row.unidadEjecutora,
-					programa: row.programa,
-					subProbrama: row.subPrograma,
-					actividad: row.actividad,
-					obra: row.obra,
-					codigo_meta: row.codigo_meta,
-					t: new Date().getTime()
-				}).then(
-				function(response){
-					if (response.data.success){
-						var datoMensual = [];
-						datoMensual = response.data.vectorValores;
-						
-						mi.datosAcumulado = [];
-						var acumuladoCant = 0;
-						var acumuladoEje = 0;
-						var acumuladoMod = 0;
-						
-						for(var i=0; i < datoMensual.length; i++){
-							if(mi.mes == datoMensual[i].mes && mi.anio == datoMensual[i].ejercicio)
-								break;
-							
-							acumuladoEje = mi.datosAcumulado.length != 0 ? mi.datosAcumulado[i-1].ejecucion + datoMensual[i].ejecucion: datoMensual[i].ejecucion;	
-							acumuladoMod = mi.datosAcumulado.length != 0 ? mi.datosAcumulado[i-1].modificacion + datoMensual[i].modificacion : datoMensual[i].modificacion;	
-							
-							if(datoMensual[i].mes == 1){
-								acumuladoCant = datoMensual[i].cantidad + acumuladoCant; 
-							}
-							
-							var p_ejecucion = acumuladoEje / (acumuladoMod + acumuladoCant);
-							mi.datosAcumulado.push({mes: datoMensual[i].mes, ejercicio: datoMensual[i].ejercicio, p_ejecucion: p_ejecucion *100, ejecucion: acumuladoEje, modificacion: acumuladoMod});	
-						}
-					}
-				});
-	}
-	
-	mi.limpiarData = function(){
-		mi.labels = [];
-		mi.data = [];
-	}
-	
-	mi.cambioAcumuladoMensual = function(){
-		mi.labels = [];
-		var cantidades = [];
-		mi.data = [];
-		
-		for(var i=0; i<mi.datoAcumuladoMensual.length; i++){
-			mi.labels.push(mi.meses[mi.datoAcumuladoMensual[i].mes - 1] + mi.datoAcumuladoMensual[i].ejercicio);
-			cantidades.push(Number(mi.datoAcumuladoMensual[i].p_ejecucion).toFixed(2));
-		}
-		
-		mi.data.push(cantidades);
-		mi.options.scales.xAxes["0"].scaleLabel.labelString = "Meses";
-	}	
-		
-	mi.cambioAcumuladoAnual = function(){
-		mi.labels = [];
-		var cantidades = [];
-		mi.data = [];
-		
-		for(var i=0; i<mi.datoAcumuladoAnual.length; i++){
-			mi.labels.push(mi.datoAcumuladoAnual[i].ejercicio);
-			cantidades.push(Number(mi.datoAcumuladoAnual[i].p_ejecucion).toFixed(2));
-		}
-		
-		mi.data.push(cantidades);
-		mi.options.scales.xAxes["0"].scaleLabel.labelString = "Años";
-	}
-	
-	mi.cambioAcumuladoMensualNormal = function(){
-		mi.labels = [];
-		var cantidades = [];
-		mi.data = [];
-		
-		for(var i=0; i<mi.datosAcumulado.length; i++){
-			mi.labels.push(mi.meses[mi.datosAcumulado[i].mes - 1] + mi.datosAcumulado[i].ejercicio);
-			cantidades.push(Number(mi.datosAcumulado[i].p_ejecucion).toFixed(2));
-		}
-		
-		mi.data.push(cantidades);
-		mi.options.scales.xAxes["0"].scaleLabel.labelString = "Meses";
-	}
-	
-	mi.options = {
-		line : { tension : 1 },
-		legend: {
-			display: true,
-			position: 'bottom',
-		},
-		scales: {
-			yAxes: [
-			{
-				id: 'y-axis-1',
-				type: 'linear',
-				display: true,
-				position: 'left',
-				ticks: {
-	        	     callback: function (value) {
-	        	    	 if (true)
-	        	    		 value = value.toFixed(2);
-	        	    	 return numeral(value).format(' 0.00')+'%'
-                   }
-				},
-				scaleLabel: {
-                    display: true,
-                    labelString: '% Ejecucion'
-                }
-			}
-			],
-			xAxes: [{
-		    	  scaleLabel: {
-                     display: true,
-                     labelString: mi.etiquetaX
-                   }
-		      }
-		      ]
-		}
-	};
-	
-	mi.datasetOverride = [{
-        yAxisID: 'y-axis-1',
-        tension: 0
-    }]
-}])
+var app = angular.module('observatorio', ['ngRoute','chart.js','smart-table','ngUtilidades','loadOnDemand',
+	'uiGmapgoogle-maps','chart.js','smart-table']);
 
-app.directive('stFilteredCollection', function () {
-	  return {
-	    require: '^stTable',
-	    link: function (scope, element, attr, ctrl) {
-	      scope.$watch(ctrl.getFilteredCollection, function(val) {
-	        scope.filteredCollection = val;
-	        
-	        scope.ctrl.totales = 0;
-	        
-	        for(x in val){
-	        	scope.ctrl.totales = scope.ctrl.totales + val[x].cantidad;
-	        }
-	      })
-	    }
-	  }
-	});
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	$locationProvider.hashPrefix('!');
+	   //$locationProvider.html5Mode(true);
+	   $routeProvider
+	   		/*.when('/main',{
+     		templateUrl : '',
+     		resolve:{
+     			main: function main(){
+     				window.location.href = '/main.jsp';
+     			}
+     		}
+     	})*/
+		 .when('/obligaciones',{
+         	template: '<div load-on-demand="\'obligacionesController\'" class="all_page"></div>'
+         })
+         .when('/producto/:entidad/:unidadejecutora/:programa/:subprograma/:actividad',{
+         	template: '<div load-on-demand="\'productoController\'" class="all_page"></div>'
+         })
+         /*.when('/salir',{
+         	templateUrl : '<div></div>',
+         	resolve:{
+         		logout: function logout($http){
+         			$http.post('/SLogout', '').then(function(response){
+	        				    if(response.data.success)
+	        				    	window.location.href = '/login.jsp';
+	        			 	}, function errorCallback(response){
+	        			 		
+	        			 	}
+	        			 );
+         			return true;
+         		}
+         	}
+         });*/
+ }]);
+
+app.config(['$loadOnDemandProvider', function ($loadOnDemandProvider) {
+	   var modules = [
+	       {
+	    	   name: 'obligacionesController',
+	    	   script: '/app/components/obligaciones/obligaciones.controller.js',
+	    	   template: '/app/components/obligaciones/obligaciones.jsp'
+	       },
+	       {
+	    	   name: 'productoController',
+	    	   script: '/app/components/producto/producto.controller.js',
+	    	   template: '/app/components/producto/producto.jsp'
+	       }
+	   ];
+	   $loadOnDemandProvider.config(modules);
+}]);
+
+app.config(['uiGmapGoogleMapApiProvider',function(uiGmapGoogleMapApiProvider) {
+ uiGmapGoogleMapApiProvider.configure({
+     key: 'AIzaSyBPq-t4dJ1GV1kdtXoVZfG7PtfEAHrhr00',
+     v: '3.', //defaults to latest 3.X anyhow
+     libraries: 'weather,geometry,visualization'
+ });
+}]);
+
+
+app.controller('MainController',['$scope','$document','$rootScope','$location','$window',
+	   function($scope,$document,$rootScope,$location,$window){
+		
+		$rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+			if (location.hostname !== "localhost" || location.hostname !== "127.0.0.1"){
+				$window.ga('create', 'UA-74443600-3', 'auto');
+	    		$window.ga('send', 'pageview', $location.path());
+			}
+	    });
+	}]);
--- Observatorio/WebContent/main.jsp
+++ Observatorio/WebContent/main.jsp
@@ -10,89 +10,10 @@
 
 <title>Observatorio</title>
 </head>
-<body ng-app="observatorio" ng-controller="mainController as ctrl">
-<div id="mainview" class="all_page">
-	<div class="col-sm-12">
-		<div class="row">
-			<div class="col-sm-3"></div>
-			<div class="col-sm-6">
-				<h3 align="center">Ejecución Física</h3>
-				<table st-table="ctrl.displayedCollection" st-safe-src="ctrl.rowCollection" class="table table-striped" st-filtered-collection>
-					<thead>
-						<tr>
-							<th>Descripción</th>
-							<th style="text-align: center">{{ctrl.ano_p_ejecucion_4}}</th>
-							<th style="text-align: center">{{ctrl.ano_p_ejecucion_3}}</th>
-							<th style="text-align: center">{{ctrl.ano_p_ejecucion_2}}</th>
-							<th style="text-align: center">{{ctrl.ano_p_ejecucion_1}}</th>
-							<th style="text-align: center">{{ctrl.ano_p_ejecucion}}</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr ng-repeat="row in ctrl.displayedCollection" ng-click="ctrl.getGraficaProducto(row);">
-							<td align="left">
-								{{row.metaDescripcion}}
-							</td>
-							<td align="right">
-								{{row.p_ejecucion_4 | formatoMillonesSinTipo : ctrl.millones}}%
-							</td>
-							<td align="right">
-								{{row.p_ejecucion_3 | formatoMillonesSinTipo : ctrl.millones}}%
-							</td>
-							<td align="right">
-								{{row.p_ejecucion_2 | formatoMillonesSinTipo : ctrl.millones}}%
-							</td>
-							<td align="right">
-								{{row.p_ejecucion_1 | formatoMillonesSinTipo : ctrl.millones}}%
-							</td>
-							<td align="right">
-								{{row.p_ejecucion | formatoMillonesSinTipo : ctrl.millones}}%
-							</td>
-						</tr>
-					</tbody>
-					<tfoot>
-						<tr>
-							<th>Totales:</th>
-							<th style="text-align: right;">
-								<div>{{ctrl.tot_p_ejecucion_4}}%</div>
-							</th>
-							<th style="text-align: right;">
-								<div>{{ctrl.tot_p_ejecucion_3}}%</div>
-							</th>
-							<th style="text-align: right;">
-								<div>{{ctrl.tot_p_ejecucion_2}}%</div>
-							</th>
-							<th style="text-align: right;">
-								<div>{{ctrl.tot_p_ejecucion_1}}%</div>
-							</th>
-							<th style="text-align: right;">
-								<div>{{ctrl.tot_p_ejecucion}}%</div>
-							</th>
-						</tr>
-					</tfoot>
-				</table>
-			</div>
-			<div class="col-sm-3"></div>
-		</div>
-		<br>
-		<h3 align="center">{{ctrl.tituloGrafica}}</h3>
-		<div align="right" style="width: 80%;margin: 0 auto; display: table;">
-			<div class="btn-group btn-group-xs	">
-				<label class="btn btn-default" ng-click="ctrl.cambioAcumuladoMensual();"><span>Mensual</span></label>
-				<label class="btn btn-default" ng-click="ctrl.cambioAcumuladoAnual();"><span>Anual</span></label>
-				<label class="btn btn-default" ng-click="ctrl.cambioAcumuladoMensualNormal();"><span>Continua</span></label>
-			</div>
-		</div>
-		<div align="center" class="row" style="width: 80%;margin: 0 auto; display: table;">
-			<div align="center">
-				<canvas id="line" class="chart chart-line" chart-data="ctrl.data"
-					chart-labels="ctrl.labels" chart-series="ctrl.series" chart-options="ctrl.options"
-					chart-dataset-override="ctrl.datasetOverride" chart-legend="true">
-				</canvas>
-			</div>
-		</div>
-	</div>
-<div class="footer" id="footer"><div style="text-align: center; width: 100%;" class="label-form">- MINFIN {{ctrl.anio}} -</div></div>
-</div>
+<body ng-app="observatorio" ng-controller="MainController as mainController">
+	<div id="mainview">
+		<div ng-view></div>
+    </div>
+    <div class="footer">- Minfin 2018 -</div>
 </body>
 </html>
