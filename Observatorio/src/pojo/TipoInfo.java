319616f14aa191827e32b0198659782f38394631
rafa <rafa@25.17.23.218>
2018-03-14 14:05:07 -0600

    Agregado TipoInfo

---
+++ Observatorio/src/pojo/TipoInfo.java
@@ -0,0 +1,9 @@
+package pojo;
+
+public class TipoInfo {
+		public double p_fisico;
+		public double p_presupuestario;
+		public double vigente;
+		public double ejecutado;
+		public int cantidad;
+}
