0ade0be8023fc9d5bfef411e0a3d3b1e2539d39c
ppenan <pedro_penan@hotmail.com>
2018-03-14 14:41:14 -0600

    Se quita la gráfica de ejecución y vigente

--- Observatorio/WebContent/app/components/programa/programa.controller.js
+++ Observatorio/WebContent/app/components/programa/programa.controller.js
@@ -6,9 +6,7 @@ angular.module('programaController',[]).controller('programaController', ['$root
 	mi.anio = fecha.getFullYear();
 	mi.mes = fecha.getMonth();
 	mi.etiquetaX = "";
-	mi.labels = [];
 	mi.labels2 = [];
-	mi.data = [];
 	mi.data2 = [];
 	mi.decimales = false;
 	mi.linealColors = ['#8ecf4c', '#88b4df', '#d92a27'];
@@ -72,11 +70,9 @@ angular.module('programaController',[]).controller('programaController', ['$root
 	})
 	
 	mi.getGraficaGeneral = function(datos){
-		mi.series = ['Vigente', 'Ejecutado'];
 		mi.series2 = ['% Financiero' , '% Físico']
 		mi.tot_asignado = 0;
 		
-		mi.labels = [];
 		mi.lables2 = [];
 		
 		mi.mensualVigente = new Array(60).fill(0);
@@ -84,8 +80,6 @@ angular.module('programaController',[]).controller('programaController', ['$root
 		mi.mensualPFinanciero = new Array(60).fill(0);
 		mi.mensualPFisico = new Array(60).fill(0);
 		
-		mi.anualVigente = [];
-		mi.anualEjecutado = [];
 		mi.anualPFinanciero = [];
 		mi.anualPFisico = [];
 		
@@ -118,12 +112,6 @@ angular.module('programaController',[]).controller('programaController', ['$root
 							if(pos==4 && h==12)
 								mi.tot_vigente += datos[i].ejercicio_data[pos][h];
 							
-							var posarr = pos==0 ? (h-1+0) : pos==1 ? (h-1+12) : pos==2 ? (h-1+24) : pos==3 ? (h-1+36) : pos==4 ? (h-1+48) : 0;
-							mi.mensualVigente[posarr] += datos[i].ejercicio_data[pos][h];
-							
-							if(h==12)
-								mi.anualVigente.push(datos[i].ejercicio_data[pos][h]);
-							
 						}else if((h>=13) && (h<=24)){ //ejecutado
 							if(pos==0 && h==24)
 								mi.tot_ejecutado_4 += datos[i].ejercicio_data[pos][h];
@@ -135,12 +123,6 @@ angular.module('programaController',[]).controller('programaController', ['$root
 								mi.tot_ejecutado_1 += datos[i].ejercicio_data[pos][h];
 							if(pos==4 && h==24)
 								mi.tot_ejecutado += datos[i].ejercicio_data[pos][h];
-							
-							var posarr = pos==0 ? (h-13+0) : pos==1 ? (h-13+12) : pos==2 ? (h-13+24) : pos==3 ? (h-13+36) : pos==4 ? (h-13+48) : 0;							
-							mi.mensualEjecutado[posarr] += datos[i].ejercicio_data[pos][h];
-							
-							if(h==24)
-								mi.anualEjecutado.push(datos[i].ejercicio_data[pos][h]);
 							
 						}else if((h>=25) && (h<=36)){ //porcentaje financiero presupuestario
 							if(pos==0 && h==36)
@@ -199,7 +181,6 @@ angular.module('programaController',[]).controller('programaController', ['$root
 					for(var j=0; j<12; j++){
 						if(mi.dato[i].ejercicios[h] == mi.anio && j==mi.mes)
 							break;
-						mi.labels.push(mi.meses[j] + mi.dato[i].ejercicios[h]);
 						mi.labels2.push(mi.meses[j] + mi.dato[i].ejercicios[h]);
 					}
 				}
@@ -211,46 +192,27 @@ angular.module('programaController',[]).controller('programaController', ['$root
 			}
 			
 			mi.tipoDatos = 1;
-			mi.data.push(mi.mensualVigente, mi.mensualEjecutado);
 			mi.data2.push(mi.mensualPFinanciero, mi.mensualPFisico);
 			
-			mi.options.scales.xAxes["0"].scaleLabel.labelString = "Meses";
 			mi.options2.scales.xAxes["0"].scaleLabel.labelString = "Meses";
 		}
 	}
 	
 	mi.getGraficaIndividual = function(row){
-		mi.labels = [];
 		mi.labels2 = [];
 		
-		mi.data = [];
 		mi.data2 = [];
 		
-		mi.mensualVigente = [];
-		mi.mensualEjecutado = [];
 		mi.mensualPFinanciero = [];
 		mi.mensualPFisico = [];
 		
-		mi.anualVigente = [];
-		mi.anualEjecutado = [];
 		mi.anualPFinanciero = [];
 		mi.anualPFisico = [];
 		
 		for(var j=0; j<row.ejercicios.length; j++){
 			var pos = row.ejercicios[j] - mi.anio + 4;
 			for(var h=0; h<row.ejercicio_data[pos].length; h++){
-				if((h>=1) && (h<=12)){//vigente
-					var posarr = pos==0 ? (h-1+0) : pos==1 ? (h-1+12) : pos==2 ? (h-1+24) : pos==3 ? (h-1+36) : pos==4 ? (h-1+48) : 0;
-					mi.mensualVigente.push(row.ejercicio_data[pos][h]);
-					
-					if(h==12)
-						mi.anualVigente.push(row.ejercicio_data[pos][h]);
-				}else if((h>=13) && (h<=24)){//ejecutado
-					mi.mensualEjecutado.push(row.ejercicio_data[pos][h]);
-					
-					if(h==24)
-						mi.anualEjecutado.push(row.ejercicio_data[pos][h]);
-				}else if((h>=25) && (h<=36)){ //porcentaje financiero presupuestario
+				if((h>=25) && (h<=36)){ //porcentaje financiero presupuestario
 					mi.mensualPFinanciero.push(row.ejercicio_data[pos][h] * 100);
 					
 					if(h==36)
@@ -269,47 +231,13 @@ angular.module('programaController',[]).controller('programaController', ['$root
 				for(var j=0; j<12; j++){
 					if(row.ejercicios[h] == mi.anio && j==mi.mes)
 						break;
-					mi.labels.push(mi.meses[j] + row.ejercicios[h]);
 					mi.labels2.push(mi.meses[j] + row.ejercicios[h]);
 				}
 			}
 		}
 		
 		mi.tipoDatos = 2;
-		mi.data.push(mi.mensualVigente, mi.mensualEjecutado);
 		mi.data2.push(mi.mensualPFinanciero, mi.mensualPFisico);
-	}
-	
-	mi.cambioMensual = function(){
-		mi.data = [];
-		mi.labels = [];
-		
-		for(var i=0; i<1; i++){
-			for(var h=0; h<mi.dato[i].ejercicios.length;h++){
-				for(var j=0; j<12; j++){
-					if(mi.dato[i].ejercicios[h] == mi.anio && j==mi.mes)
-						break;
-					mi.labels.push(mi.meses[j] + mi.dato[i].ejercicios[h]);
-				}
-			}
-		}
-		
-		mi.options.scales.xAxes["0"].scaleLabel.labelString = "Meses";
-		mi.data.push(mi.mensualVigente, mi.mensualEjecutado);	
-	}
-	
-	mi.cambioAnual =  function(){
-		mi.labels = [];
-		mi.data = [];
-
-		for(var i=0; i<1; i++){
-			for(var h=0; h<mi.dato[i].ejercicios.length;h++){
-				mi.labels.push(mi.dato[i].ejercicios[h]);
-			}
-		}
-		
-		mi.options.scales.xAxes["0"].scaleLabel.labelString = "Años";
-		mi.data.push(mi.anualVigente, mi.anualEjecutado);
 	}
 	
 	mi.cambioMensualP = function(){
@@ -343,58 +271,6 @@ angular.module('programaController',[]).controller('programaController', ['$root
 		mi.options2.scales.xAxes["0"].scaleLabel.labelString = "Años";
 		mi.data2.push(mi.anualPFinanciero, mi.anualPFisico);
 	}
-	
-	mi.options = {
-			elements: {
-		        line: {
-		            tension: 0
-		        },
-		        point:{
-		        	radius: 5
-		        }
-		    },
-			legend: {
-				display: true,
-				position: 'bottom',
-			},
-			scales: {
-				yAxes: [
-				{
-					id: 'y-axis-1',
-					type: 'linear',
-					display: true,
-					position: 'left',
-					ticks: {
-		        	     callback: function (value) {
-		        	    	 if (true)
-		        	    		 value = value.toFixed(2);
-		        	    	 return 'Q' + numeral(value).format('0,000.00')
-	                   }
-					},
-					scaleLabel: {
-	                    display: true,
-	                    labelString: 'Q Ejecucion'
-	                }
-				}
-				],
-				xAxes: [{
-			    	  scaleLabel: {
-	                     display: true,
-	                     labelString: mi.etiquetaX
-	                   }
-			      }
-			      ]
-			},
-			tooltips: {
-		           mode: 'label',
-		           label: 'mylabel',
-		           callbacks: {
-		               label: function(tooltipItem, data) {
-		                   return 'Q ' + numeral(tooltipItem.yLabel).format('0,000.00'); 
-		               } 
-		           }
-		        }
-		};
 	
 	mi.options2 = {
 			elements: {
