bdc1a768b4d69f8baf384c6cd3d3f5972f8272dc
ppenan <pedro_penan@hotmail.com>
2018-03-13 19:24:43 -0600

    Se modifica query de Entidad y se agrega TipoResultado

---
+++ Observatorio/src/dao/TipoResultadoDAO.java
@@ -0,0 +1,161 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import utilities.CLogger;
+import utilities.CMemsql;
+
+public class TipoResultadoDAO {
+	
+	public class TipoResultado{
+		double p_fisico;
+		double p_presupuestario;
+		double vigente;
+		double ejecutado;
+		int tipo;
+	}
+	public static ArrayList<TipoResultado> getTipoResultado(Integer ejercicio, String tipo_resultado){
+		ArrayList<TipoResultado> ret = new ArrayList<TipoResultado>();
+		String query = "";
+		try{
+			if(CMemsql.connect()){
+				query = String.join(" ", "select ", 
+						"  case month(CURRENT_TIMESTAMP())", 
+						"    when 1 then sum(financiero_ejecutado_m1)", 
+						"    when 2 then sum(financiero_ejecutado_m2)", 
+						"    when 3 then sum(financiero_ejecutado_m3)", 
+						"    when 4 then sum(financiero_ejecutado_m4)", 
+						"    when 5 then sum(financiero_ejecutado_m5)", 
+						"    when 6 then sum(financiero_ejecutado_m6)", 
+						"    when 7 then sum(financiero_ejecutado_m7)", 
+						"    when 8 then sum(financiero_ejecutado_m8)", 
+						"    when 9 then sum(financiero_ejecutado_m9)", 
+						"    when 10 then sum(financiero_ejecutado_m10)", 
+						"    when 11 then sum(financiero_ejecutado_m11)", 
+						"    when 12 then sum(financiero_ejecutado_m12)", 
+						"  end ejecutado,", 
+						"  case month(CURRENT_TIMESTAMP())", 
+						"    when 1 then sum(financiero_vigente_m1)", 
+						"    when 2 then sum(financiero_vigente_m2)", 
+						"    when 3 then sum(financiero_vigente_m3)", 
+						"    when 4 then sum(financiero_vigente_m4)", 
+						"    when 5 then sum(financiero_vigente_m5)", 
+						"    when 6 then sum(financiero_vigente_m6)", 
+						"    when 7 then sum(financiero_vigente_m7)", 
+						"    when 8 then sum(financiero_vigente_m8)", 
+						"    when 9 then sum(financiero_vigente_m9)", 
+						"    when 10 then sum(financiero_vigente_m10)", 
+						"    when 11 then sum(financiero_vigente_m11)", 
+						"    when 12 then sum(financiero_vigente_m12)", 
+						"  end vigente,", 
+						"  case month(CURRENT_TIMESTAMP())", 
+						"    when 1 then sum(financiero_ejecutado_m1)/sum(financiero_vigente_m1)", 
+						"    when 2 then sum(financiero_ejecutado_m2)/sum(financiero_vigente_m2)", 
+						"    when 3 then sum(financiero_ejecutado_m3)/sum(financiero_vigente_m3)", 
+						"    when 4 then sum(financiero_ejecutado_m4)/sum(financiero_vigente_m4)", 
+						"    when 5 then sum(financiero_ejecutado_m5)/sum(financiero_vigente_m5)", 
+						"    when 6 then sum(financiero_ejecutado_m6)/sum(financiero_vigente_m6)", 
+						"    when 7 then sum(financiero_ejecutado_m7)/sum(financiero_vigente_m7)", 
+						"    when 8 then sum(financiero_ejecutado_m8)/sum(financiero_vigente_m8)", 
+						"    when 9 then sum(financiero_ejecutado_m9)/sum(financiero_vigente_m9)", 
+						"    when 10 then sum(financiero_ejecutado_m10)/sum(financiero_vigente_m10)", 
+						"    when 11 then sum(financiero_ejecutado_m11)/sum(financiero_vigente_m11)", 
+						"    when 12 then sum(financiero_ejecutado_m12)/sum(financiero_vigente_m12)", 
+						"  end p_financiero,", 
+						"  case month(CURRENT_TIMESTAMP())", 
+						"    when 1 then sum(p_fisico_m1 * financiero_vigente_m1) / sum(financiero_vigente_m1)", 
+						"    when 2 then sum(p_fisico_m2 * financiero_vigente_m2) / sum(financiero_vigente_m2)", 
+						"    when 3 then sum(p_fisico_m3 * financiero_vigente_m3) / sum(financiero_vigente_m3)", 
+						"    when 4 then sum(p_fisico_m4 * financiero_vigente_m4) / sum(financiero_vigente_m4)", 
+						"    when 5 then sum(p_fisico_m5 * financiero_vigente_m5) / sum(financiero_vigente_m5)", 
+						"    when 6 then sum(p_fisico_m6 * financiero_vigente_m6) / sum(financiero_vigente_m6)", 
+						"    when 7 then sum(p_fisico_m7 * financiero_vigente_m7) / sum(financiero_vigente_m7)", 
+						"    when 8 then sum(p_fisico_m8 * financiero_vigente_m8) / sum(financiero_vigente_m8)", 
+						"    when 9 then sum(p_fisico_m9 * financiero_vigente_m9) / sum(financiero_vigente_m9)", 
+						"    when 10 then sum(p_fisico_m10 * financiero_vigente_m10) / sum(financiero_vigente_m10)", 
+						"    when 11 then sum(p_fisico_m11 * financiero_vigente_m11) / sum(financiero_vigente_m11)", 
+						"    when 12 then sum(p_fisico_m12 * financiero_vigente_m12) / sum(financiero_vigente_m12)", 
+						"  end pp_fisico", 
+						"	from (  ", 
+						"	  select ejercicio, entidad, entidad_nombre, unidad_ejecutora, unidad_ejecutora_nombre, programa, programa_nombre, subprograma, proyecto, obra, actividad, actividad_obra_nombre, tipo_resultado, ", 
+						"	  avg(financiero_asignado) financiero_asignado,  ", 
+						"	  avg(financiero_ejecutado_m1) financiero_ejecutado_m1,  ", 
+						"	  avg(financiero_ejecutado_m2) financiero_ejecutado_m2,  ", 
+						"	  avg(financiero_ejecutado_m3) financiero_ejecutado_m3,  ", 
+						"	  avg(financiero_ejecutado_m4) financiero_ejecutado_m4,  ", 
+						"	  avg(financiero_ejecutado_m5) financiero_ejecutado_m5,  ", 
+						"	  avg(financiero_ejecutado_m6) financiero_ejecutado_m6,  ", 
+						"	  avg(financiero_ejecutado_m6) financiero_ejecutado_m7,  ", 
+						"	  avg(financiero_ejecutado_m7) financiero_ejecutado_m8,  ", 
+						"	  avg(financiero_ejecutado_m8) financiero_ejecutado_m9,  ", 
+						"	  avg(financiero_ejecutado_m9) financiero_ejecutado_m10,  ", 
+						"	  avg(financiero_ejecutado_m10) financiero_ejecutado_m11,  ", 
+						"	  avg(financiero_ejecutado_m12) financiero_ejecutado_m12,  ", 
+						"	  avg(financiero_vigente_m1) financiero_vigente_m1,  ", 
+						"	  avg(financiero_vigente_m2) financiero_vigente_m2,  ", 
+						"	  avg(financiero_vigente_m3) financiero_vigente_m3,  ", 
+						"	  avg(financiero_vigente_m4) financiero_vigente_m4,  ", 
+						"	  avg(financiero_vigente_m5) financiero_vigente_m5,  ", 
+						"	  avg(financiero_vigente_m6) financiero_vigente_m6,  ", 
+						"	  avg(financiero_vigente_m7) financiero_vigente_m7,  ", 
+						"	  avg(financiero_vigente_m8) financiero_vigente_m8,  ", 
+						"	  avg(financiero_vigente_m9) financiero_vigente_m9,  ", 
+						"	  avg(financiero_vigente_m10) financiero_vigente_m10,  ", 
+						"	  avg(financiero_vigente_m11) financiero_vigente_m11,  ", 
+						"	  avg(financiero_vigente_m12) financiero_vigente_m12,  ", 
+						"	  avg(financiero_ejecutado_m1)/ifnull(avg(if(financiero_vigente_m1>0,financiero_vigente_m1,1)),1) p_financiero_m1,  ", 
+						"	  avg(financiero_ejecutado_m2)/ifnull(avg(if(financiero_vigente_m2>0,financiero_vigente_m2,1)),1) p_financiero_m2,  ", 
+						"	  avg(financiero_ejecutado_m3)/ifnull(avg(if(financiero_vigente_m3>0,financiero_vigente_m3,1)),1) p_financiero_m3,  ", 
+						"	  avg(financiero_ejecutado_m4)/ifnull(avg(if(financiero_vigente_m4>0,financiero_vigente_m4,1)),1) p_financiero_m4,  ", 
+						"	  avg(financiero_ejecutado_m5)/ifnull(avg(if(financiero_vigente_m5>0,financiero_vigente_m5,1)),1) p_financiero_m5,  ", 
+						"	  avg(financiero_ejecutado_m6)/ifnull(avg(if(financiero_vigente_m6>0,financiero_vigente_m6,1)),1) p_financiero_m6,  ", 
+						"	  avg(financiero_ejecutado_m7)/ifnull(avg(if(financiero_vigente_m7>0,financiero_vigente_m7,1)),1) p_financiero_m7,  ", 
+						"	  avg(financiero_ejecutado_m8)/ifnull(avg(if(financiero_vigente_m8>0,financiero_vigente_m8,1)),1) p_financiero_m8,  ", 
+						"	  avg(financiero_ejecutado_m9)/ifnull(avg(if(financiero_vigente_m9>0,financiero_vigente_m9,1)),1) p_financiero_m9,  ", 
+						"	  avg(financiero_ejecutado_m10)/ifnull(avg(if(financiero_vigente_m10>0,financiero_vigente_m10,1)),1) p_financiero_m10,  ", 
+						"	  avg(financiero_ejecutado_m11)/ifnull(avg(if(financiero_vigente_m11>0,financiero_vigente_m11,1)),1) p_financiero_m11,  ", 
+						"	  avg(financiero_ejecutado_m12)/ifnull(avg(if(financiero_vigente_m12>0,financiero_vigente_m12,1)),1) p_financiero_m12,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m1, IF (fisico_asignado + IFNULL(fisico_modificacion_m1,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m1,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m1,0),1))) p_fisico_m1,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m2, IF (fisico_asignado + IFNULL(fisico_modificacion_m2,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m2,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m2,0),1))) p_fisico_m2,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m3, IF (fisico_asignado + IFNULL(fisico_modificacion_m3,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m3,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m3,0),1))) p_fisico_m3,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m4, IF (fisico_asignado + IFNULL(fisico_modificacion_m4,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m4,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m4,0),1))) p_fisico_m4,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m5, IF (fisico_asignado + IFNULL(fisico_modificacion_m5,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m5,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m5,0),1))) p_fisico_m5,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m6, IF (fisico_asignado + IFNULL(fisico_modificacion_m6,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m6,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m6,0),1))) p_fisico_m6,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m7, IF (fisico_asignado + IFNULL(fisico_modificacion_m7,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m7,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m7,0),1))) p_fisico_m7,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m8, IF (fisico_asignado + IFNULL(fisico_modificacion_m8,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m8,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m8,0),1))) p_fisico_m8,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m9, IF (fisico_asignado + IFNULL(fisico_modificacion_m9,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m9,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m9,0),1))) p_fisico_m9,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m10, IF (fisico_asignado + IFNULL(fisico_modificacion_m10,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m10,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m10,0),1))) p_fisico_m10,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m11, IF (fisico_asignado + IFNULL(fisico_modificacion_m11,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m11,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m11,0),1))) p_fisico_m11,  ", 
+						"	(AVG(IFNULL(fisico_ejecutado_m12, IF (fisico_asignado + IFNULL(fisico_modificacion_m12,0) > 0, 0, NULL)) / IF (fisico_asignado + IFNULL(fisico_modificacion_m12,0) > 0,fisico_asignado + IFNULL(fisico_modificacion_m12,0),1))) p_fisico_m12  ", 
+						"	  from mv_financiera_fisica   ", 
+						"	  where tipo_resultado=? and ejercicio=? ", 
+						"	  group by entidad, unidad_ejecutora, programa, subprograma, proyecto, actividad,ejercicio  ", 
+						"	  order by entidad, unidad_ejecutora, programa, subprograma, proyecto, actividad,ejercicio  ", 
+						"	) t1  ", 
+						"	group by ejercicio, tipo_resultado;");
+				
+				PreparedStatement pstmt = CMemsql.getConnection().prepareStatement(query);
+				pstmt.setString(1, tipo_resultado);
+				pstmt.setInt(2, ejercicio);
+				
+				ResultSet rs = CMemsql.runPreparedStatement(pstmt);
+				
+				while(rs.next()){
+					TipoResultado temp = (new TipoResultadoDAO()).new TipoResultado();
+					temp.ejecutado = rs.getDouble("ejecutado");
+					temp.vigente = rs.getDouble("vigente");
+					temp.p_presupuestario = rs.getDouble("p_financiero");
+					temp.p_fisico = rs.getDouble("pp_fisico");
+					ret.add(temp);
+				}
+			}
+			
+			return ret;
+		}catch(Exception e){
+			CLogger.write("1", TipoResultadoDAO.class, e);
+			return ret;
+		}
+	}
+}
---
+++ Observatorio/src/servlets/STipoResultado.java
@@ -0,0 +1,76 @@
+package servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.zip.GZIPOutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import dao.TipoResultadoDAO;
+import dao.TipoResultadoDAO.TipoResultado;
+import utilities.CLogger;
+import utilities.Utils;
+
+@WebServlet("/STipoResultado")
+public class STipoResultado extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public STipoResultado() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		Gson gson = new Gson();
+		Type type = new TypeToken<Map<String, String>>(){}.getType();
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = request.getReader();
+		String str;
+		while ((str = br.readLine()) != null) {
+			sb.append(str);
+		};
+		Map<String, String> map = gson.fromJson(sb.toString(), type);
+		String accion = map.get("accion");
+		String response_text="";
+		
+		Integer ejercicio = Utils.String2Int(map.get("ejercicio"));
+		String tipo_resultado = Utils.String2Int(map.get("tipo_resultado")) == 1 ? "Estrátegico" : (Utils.String2Int(map.get("tipo_resultado")) == 2 ? "Institucional" : "Otros");
+		
+		if (accion.equals("getTipoResultado")){
+			try{
+				ArrayList<TipoResultado> lsttiporesultado = TipoResultadoDAO.getTipoResultado(ejercicio, tipo_resultado);
+				String tipoResultado = new GsonBuilder().serializeNulls().create().toJson(lsttiporesultado);
+				response_text = String.join(" ", "\"tiporesultado\": ", tipoResultado);
+				response_text = String.join(" ","{\"success\": true,", response_text, "}");
+			}catch(Exception e){
+				CLogger.write("1", STipoResultado.class, e);
+			}
+		}
+		
+		response.setHeader("Content-Encoding", "gzip");
+		response.setCharacterEncoding("UTF-8");
+
+        OutputStream output = response.getOutputStream();
+		GZIPOutputStream gz = new GZIPOutputStream(output);
+        gz.write(response_text.getBytes("UTF-8"));
+        gz.close();
+        output.close();
+	}
+
+}
