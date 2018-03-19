3fa049a4522fdc90a111b8a5191695a11189555b
rafa <rafa@25.17.23.218>
2018-03-19 13:12:41 -0600

    Cambios a salud

--- Observatorio/src/dao/SaludDAO.java
+++ Observatorio/src/dao/SaludDAO.java
@@ -11,6 +11,7 @@ import utilities.CMemsql;
 
 public class SaludDAO {
 	public class Hospital{
+		Integer codigo;
 		String nombre;
 		Integer treeLevel;
 		Double asignado;
@@ -33,27 +34,55 @@ public class SaludDAO {
 				DateTime init_year = new DateTime().minusYears(4);
 				int departamento_actual = 0;
 				int hospital_actual = 0;
+				Double u_asignado=0.0d, d_asignado=0.0d;
+				Double u_vigente=0.0d, d_vigente=0.0d;
+				Double u_ejecutado=0.0d, d_ejecutado=0.0d;
 				Hospital temp=null;
 				while(rs.next()){
 					if(rs.getInt("codigo_departamento")!=departamento_actual){
 						Hospital depto = (new SaludDAO()).new Hospital();
 						depto.treeLevel = 0;
 						depto.nombre = rs.getString("nombre_departamento");
+						depto.asignado = d_asignado;
+						depto.vigente = d_vigente;
+						depto.ejecutado = d_ejecutado;
 						ret.add(depto);
+						d_asignado=d_vigente=d_ejecutado=0.0d;
 					}
 					if(rs.getInt("unidad_ejecutora")!=hospital_actual){
-						if(temp!=null)
+						if(temp!=null){
 							ret.add(temp);
+							int pos_hospital_anterior = ret.size()-1;
+							while(ret.get(pos_hospital_anterior).codigo==hospital_actual)
+								pos_hospital_anterior--;
+							pos_hospital_anterior++;
+							ret.get(pos_hospital_anterior+1).asignado = ret.get(pos_hospital_anterior+2).asignado;
+							ret.get(pos_hospital_anterior+1).asignado += ret.get(pos_hospital_anterior+3).asignado;
+							ret.get(pos_hospital_anterior+1).asignado += ret.get(pos_hospital_anterior+4).asignado;
+							ret.get(pos_hospital_anterior+1).vigente = ret.get(pos_hospital_anterior+2).vigente;
+							ret.get(pos_hospital_anterior+1).vigente += ret.get(pos_hospital_anterior+3).vigente;
+							ret.get(pos_hospital_anterior+1).vigente += ret.get(pos_hospital_anterior+4).vigente;
+							ret.get(pos_hospital_anterior+1).ejecutado = ret.get(pos_hospital_anterior+2).ejecutado;
+							ret.get(pos_hospital_anterior+1).ejecutado += ret.get(pos_hospital_anterior+3).ejecutado;
+							ret.get(pos_hospital_anterior+1).ejecutado += ret.get(pos_hospital_anterior+4).asignado;
+						}
 						temp = (new SaludDAO()).new Hospital();
+						temp.codigo = rs.getInt("unidad_ejecutora");
 						temp.nombre = rs.getString("nombre");
-						temp.treeLevel = rs.getInt("nivel");
+						temp.treeLevel = 1;
 						temp.ejercicios = new Integer[5];
-						temp.data_ejercicio = new ArrayList<Double[]>(5);
+						temp.data_ejercicio = new ArrayList<Double[]>();
+						for(int i=0; i<5; i++)
+							temp.data_ejercicio.add(new Double[3]);
+						hospital_actual = temp.codigo;
 					}
+					temp.codigo = rs.getInt("rubro");
+					temp.nombre = rs.getString("nombre_rubro");
+					temp.treeLevel = rs.getInt("nivel") + 1;
 					temp.ejercicios[rs.getInt("ejercicio")-init_year.getYear()]=rs.getInt("ejercicio");
 					temp.data_ejercicio.get(rs.getInt("ejercicio")-init_year.getYear())[0] = rs.getDouble("asignado");
-					temp.data_ejercicio.get(rs.getInt("ejercicio")-init_year.getYear())[0] = rs.getDouble("vigente");
-					temp.data_ejercicio.get(rs.getInt("ejercicio")-init_year.getYear())[0] = rs.getDouble("ejecucion");
+					temp.data_ejercicio.get(rs.getInt("ejercicio")-init_year.getYear())[1] = rs.getDouble("vigente");
+					temp.data_ejercicio.get(rs.getInt("ejercicio")-init_year.getYear())[2] = rs.getDouble("ejecucion");
 				}
 				if(temp!=null)
 					ret.add(temp);
